6f9cc2f12a7c3323e2d795753cdd8a13589e4ee0
AncutaBiro <ancuta.biro@yahoo.com>
2020-05-10 00:02:46 +0300

    Pragmatic Ex - Repetition - ex 30-31

---
+++ src/main/java/org/fasttrackit/Repetition/KarvonenHeartRate.java
@@ -0,0 +1,42 @@
+package org.fasttrackit.Repetition;
+
+import org.fasttrackit.utils.ScannerUtils;
+
+import javax.sound.midi.Soundbank;
+import java.util.InputMismatchException;
+
+public class KarvonenHeartRate {
+
+    public static void main(String[] args) {
+
+        System.out.print("Resting pulse: ");
+        int restingHR = inputValidator();
+
+        System.out.print("Age: ");
+        int age = inputValidator();
+
+        System.out.println();
+
+        System.out.println("Intensity | Rate");
+        System.out.println("_ _ _ _ _ _ _ _ _ _");
+
+        for (int i = 55; i <= 95; i+=5) {
+            int tHR = (((220 - age) - restingHR) * i/100) + restingHR;
+            System.out.println(i + "%" + "       | " + tHR + " bpm");
+            }
+        }
+
+    private static int inputValidator (){
+        String input = ScannerUtils.nextLine();
+        try {
+            if (input.matches("[a-zA-Z]+")) {
+                throw new InputMismatchException();
+            }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Sorry. That's not a valid input.");
+            return inputValidator();
+        } return Integer.parseInt(input);
+    }
+
+}
---
+++ src/main/java/org/fasttrackit/Repetition/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package org.fasttrackit.Repetition;
+
+import org.fasttrackit.utils.ScannerUtils;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 12; i++) {
+
+            for (int j = 0; j <= 12; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+        }
+
+    }
+
+}
